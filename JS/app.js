b1305c099945332c3be785f1ab9392846e599c5e
Tom <capps14e@gmail.com>
2023-09-21 16:53:52 -0400

    restructured appjs file, fixed formatting

--- JS/app.js
+++ JS/app.js
@@ -1,56 +1,56 @@
 'use strict';
+console.log('js is connected');
 
+let parentElement = document.getElementById('cookieTable');
 
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 const seattle = {
-    locationName: 'Seattle',
-    minCustomersPerHour: 23,
-    maxCustomersPerHour: 65,
-    customersEachHour: [],
-    cookiesEachHour: [],
-    totalDailyCookies: 0,
-    avgCookiesPerSale: 4.6,
-    calcCustomerEachHour: function () {
-        for (let i = 0; i < hours.length; i++) {
-            this.customersEachHour.push(random(this.maxCustomersPerHour, this.minCustomersPerHour));
-        }
-    },
-    calcCookiesEachHour: function () {
-        this.calcCustomerEachHour();
-        for (let i = 0; i < hours.length; i++) {
-            console.log();
-            let oneHour = Math.ceil(this.customersEachHour[i] * this.avgCookiesPerSale);
-            console.log('for each hour', oneHour);
-            this.cookiesEachHour.push(oneHour);
-            this.totalDailyCookies = this.totalDailyCookies + oneHour;
-        }
-    },
-    render() {
-        this.calcCookiesEachHour();
-        const unorderedList = document.getElementById('city');
-        for (let i = 0; i < hours.length; i++) {
-            const listItem = document.createElement('li');
-            listItem.textContent = hours[i] + ': ' + this.cookiesEachHour[i] + ' cookies';
-            unorderedList.appendChild(listItem);
-        }
-        const listItem = document.createElement('li');
-        listItem.textContent = 'Total: ' + this.totalDailyCookies + ' cookies';
-        unorderedList.appendChild(listItem);
+  locationName: 'Seattle',
+  minCustomersPerHour: 23,
+  maxCustomersPerHour: 65,
+  hourlyTotal: [],
+  cookiesEachHour: [],
+  totalDailyCookies: 0,
+  avgCookiesPerSale: 4.6,
+  // here and below reuse code for different locations
+  calcCustomerEachHour: function () {
+    for (let i = 0; i < hours.length; i++) {
+      this.cookiesEachHour[i] = hours[i] + ' ' + Math.floor(this.avgCookiesPerSale * randomCustomer(this.minCustomersPerHour, this.maxCustomersPerHour));
     }
+  },
+  calcCookiesEachHour: function () {
+    let article = document.createElement('article');
+    parentElement.appendChild(article);
+    let storeComment = document.createElement('p');
+    storeComment.textContent = 'Cookie Sales in Seattle';
+    article.appendChild(storeComment);
+    let storeList = document.createElement('ul');
+    for (let i = 0; i < hours.length; i++) {
+      let storeL = document.createElement('li');
+      seattle.totalDailyCookies += parseInt(seattle.cookiesEachHour[i].split(' ')[1]);
+      storeList.textContent = seattle.cookiesEachHour[i];
+      storeL.appendChild(storeList);
+    }
+    article.appendChild(storeList);
+    let listItem = document.createElement('li');
+    listItem.textContent = 'total ' + seattle.totalDailyCookies + ' cookies';
+    article.appendChild(listItem);
+  }
 };
-
-
-function random(max, min) {
-    return Math.round(Math.random() * (max - min + 1)) + min;
+/// Do not copy below!
+function randomCustomer(minCustomersPerHour, maxCustomersPerHour) {
+  return Math.floor(Math.random() * (minCustomersPerHour - maxCustomersPerHour) + minCustomersPerHour);
 }
-//                0
+
+
+
+
 let allShops = [seattle];
 function renderAllShops() {
-    for (let i = 0; i < allShops.length; i++) {
-        allShops[i].render();
-    }
+  for (let i = 0; i < allShops.length; i++) {
+    allShops[i].render();
+  }
 }
 
 renderAllShops();
-}
